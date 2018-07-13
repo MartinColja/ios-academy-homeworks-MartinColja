26be304a7a10d3be7ac62afa14d48142f99ffde2
Infinum Academy <infinumstudentacademy@infinums-macbook-air-5.local>
2018-07-13 22:07:57 +0200

    - removed all UI elements from homework1 and their functionality - added imageView - added AppIcon to xcassets

--- TVShows/TVShows/New Group/LoginViewController.swift
+++ TVShows/TVShows/New Group/LoginViewController.swift
@@ -2,28 +2,16 @@
 //  LoginViewController.swift
 //  TVShows
 //
-//  Created by Infinum Student Academy on 08/07/2018.
+//  Created by Infinum Student Academy on 13/07/2018.
 //  Copyright © 2018 Sifon.co. All rights reserved.
 //
 
 import UIKit
 
 class LoginViewController: UIViewController {
-    
-    var numberOfTaps:Int = 0
 
     override func viewDidLoad() {
-        label.text="\(numberOfTaps)"
-        spinner.hidesWhenStopped = true
-        spinnerButton.backgroundColor = UIColor.red
-        spinnerButton.isEnabled = false
-        spinnerButton.setTitle("Loading", for: .disabled)
-        spinner.startAnimating()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
-            self.spinner.stopAnimating()
-            self.spinnerButton.isEnabled = true
-            self.spinnerButton.backgroundColor = UIColor.green
-        }
+        super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
@@ -33,32 +21,7 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func OnButtonClick(_ sender: Any) {
-        numberOfTaps+=1
-        label.text="\(numberOfTaps)"
-    }
-    
-    
-    @IBOutlet weak var spinnerButton: UIButton!
-    
-    @IBOutlet weak var spinner: UIActivityIndicatorView!
-    
-    @IBOutlet weak var label: UILabel!
-    
-    @IBAction func toggleSpinner(_ sender: UIButton) {
-        
-        let currentTitle: String? = spinnerButton.currentTitle
-        
-        if (currentTitle == "Start"){
-            spinnerButton.backgroundColor = UIColor.red
-            spinnerButton.setTitle("Stop", for: .normal)
-            spinner.startAnimating()
-        } else {
-            spinnerButton.backgroundColor = UIColor.green
-            spinnerButton.setTitle("Start", for: .normal)
-            spinner.stopAnimating()
-        }
-    }
+
     /*
     // MARK: - Navigation
 
